9ef05c317871e4625c65ada0f09f55dee34e1066
Luis <m.luishernandez@outlook.com>
2021-02-26 14:49:59 -0500

    added inputsearch

--- src/App.js
+++ src/App.js
@@ -8,21 +8,45 @@ import SearchForm from "./components/SearchForm"
 
 class App extends React.Component {
     state = {
-        employees: []
+        employees: [],
+        employeeFilter: []
     }
+
+    
+   handleInputSearch = (event) => {
+     const filter = event.target.value
+     const filteredList = this.state.employees.filter((user) => {
+      //  The Object.values() method returns an array of a given object's own enumerable property values, in the same order as that provided by a for...in loop. (The only difference is that a for...in loop enumerates properties in the prototype chain as well.)
+      let values = Object.values(user).join("").toLocaleLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1
+     });
+     this.setState({ employeeFilter: filteredList})
+   }
+
+
+
+
     componentDidMount() {
         API.getUsers()
         .then(res => {
-            this.setState({ employees: res.data.results })
+            this.setState({ 
+              employees: res.data.results,
+              employeeFilter: res.data.resutls
+            })
             console.log(res)
         })
             .catch(err => console.error(err.message))
     }
+
+    
+
+  
+
     render() {
         return (
             <Wrapper>
                 <Jumbotron />
-                <SearchForm />
+                <SearchForm handleInputSearch={this.handleInputSearch}/>
                 <br/>
                 <InfoTable employees={this.state.employees} />
             </Wrapper>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,18 @@
-const SearchForm = () => {
+import React from "react";
+const SearchForm = ({handleInputSearch}) => {
     return ( 
         <form className="form-inline float-right">
             <div className="form-group mb-2">
             </div>
             <div className="form-group mx-sm-3 mb-2">
                 <label htmlFor="inputSearch" className="sr-only">Search</label>
-                <input type="text" className="form-control" id="InputSearch2" placeholder="Search" />
+                <input 
+                  className="form-control"
+                  type="text"
+                  placeholder="Search Employee"
+                //   id="search"
+                  onChange={e => handleInputSearch(e)}
+             />
             </div>
             <button type="submit" className="btn btn-primary mb-2">Search</button>
         </form>
